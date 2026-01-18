3adf9f464cfef9dcb47658cae036445031991cd6
erikenander95 <erik.enander95@gmail.com>
2026-01-18 19:01:50 +0100

    Initial project setup with main.c and build script
    Added MVP.txt, build.bat for compiling with clang and raylib, and main.c with basic window initialization and FPS display using raylib. Sets up the foundation for further development.

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "raylib/raylib.h"
+#include <math.h>
+#include <stdlib.h>
+
+
+#define SCREEN_WIDTH 1920
+#define SCREEN_HEIGHT 1080
+#define TILE_SIZE 10
+
+typedef enum{
+    TILE_WATER = 0,
+    TILE_LAND = 1
+} TileType;
+
+int main(){
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Divine Intervention");
+
+    const int worldWidth = SCREEN_WIDTH / TILE_SIZE;
+    const int worldHeight = SCREEN_HEIGHT / TILE_SIZE;
+
+    while(!WindowShouldClose())
+    {
+        ClearBackground(BLUE);
+        BeginDrawing();
+
+        DrawFPS(16, 16);
+        EndDrawing();
+    }
+}
